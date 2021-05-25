42e013d5db977b98901648eeafee44493974cfad
Luis Villamizar <69737158+luisvzr@users.noreply.github.com>
2021-05-25 04:38:46 -0300

    nicer

--- organizer.py
+++ organizer.py
@@ -1,22 +1,8 @@
 import os
 import shutil
 
-# Archivos de downloads:
-print("Please insert your Downloads folder path:")
-source = input()
-#source = "E:/Share/Downloads"
-files = os.listdir(source)
 
-# tuples
-pictures = (".jpg", ".png")
-music = (".mp3", ".m4a")
-video = (".mp4", ".mkv")
-app = (".exe", ".msi")
-compressed = (".zip", ".rar", ".ZIP")
-docs = (".pdf", ".doc", ".docx", ".xls")
-isos = ".iso"
-
-# folders:
+source = input("Please insert your Downloads full folder path:\n")
 root_path = "E:/Share/"
 
 app_folder = "Downloads/Installers"
@@ -28,42 +14,31 @@ docs_folder = "Documents"
 isos_folder = "ISOS"
 subs_folder = "Subs"
 
+dictionary = {
+    app_folder: (".exe", ".msi"),
+    music_folder: (".mp3", ".m4a"),
+    pictures_folder: (".jpg", ".png"),
+    video_folder: (".mp4", ".mkv"),
+    compressed_folder: (".zip", ".rar", ".ZIP"),
+    docs_folder: (".pdf", ".doc", ".docx", ".xls"),
+    isos_folder: ".iso"
+}
 
+def create(dir):
+    dir_name = "E:/Share/%s" % (dir)
+    try:
+        os.mkdir(dir_name)
+    except FileExistsError:
+        pass
 
-folders = [app_folder, music_folder, pictures_folder, video_folder, compressed_folder, docs_folder, subs_folder]
+def move_file(file,dir):
+    dest = str(root_path + dir +"/"+ file)
+    src = str(source+ "/" + file)
+    shutil.move(src , dest)
+    print("[MOVING] %s/%s" % (dir, file))
 
-for folder in folders:
-    isFile = os.path.isfile(os.path.join(root_path,folder))
-    if isFile == True:
-        print(folder, " Exist")
-    else:    
-        os.makedirs(os.path.join(root_path,folder),exist_ok=True)  
-
-for f in files:
-    #print(str(root_path+music_folder+"/"+f))
-    if f.endswith(tuple(music)):
-        shutil.move(source+ "/" + f,(str(root_path+music_folder+"/"+f)))
-        print(f, "moved succesfully!")
-    elif f.endswith(tuple(pictures)):
-        shutil.move(source + "/" + f,(str(root_path+pictures_folder+"/"+f)))
-        print(f, "moved succesfully!")
-    elif f.endswith(tuple(compressed)):
-        shutil.move(source + "/" + f,(str(root_path+compressed_folder+"/"+f)))
-        print(f, "moved succesfully!")
-    elif f.endswith(tuple(app)):
-        shutil.move(source + "/" + f,(str(root_path+app_folder+"/"+f)))
-        print(f, "moved succesfully!")
-    elif f.endswith(tuple(video)):
-        shutil.move(source + "/" + f,(str(root_path+video_folder+"/"+f)))
-        print(f, "moved succesfully!")
-    elif f.endswith(tuple(docs)):
-        shutil.move(source + "/" + f,(str(root_path+docs_folder+"/"+f)))
-        print(f, "moved succesfully!")
-    elif f.endswith(isos):
-        shutil.move(source + "/" + f,(str(root_path+isos_folder+"/"+f)))
-        print(f, "moved succesfully!")
-    elif f.endswith(".srt"):
-        shutil.move(source + "/" + f,(str(root_path+subs_folder+"/"+f)))
-    elif os.path.isfile(str(source+f)) == True:
-        print(f, " file type not recognized, ignored")
-print("done!")
+for f in os.listdir(source):
+    for dir in dictionary:
+        create(dir)
+        if f.endswith(dictionary[dir]):
+            move_file(f,dir)
